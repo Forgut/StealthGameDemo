90147945e669be02d082ee948d14b4e874a3e2ce
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-30 12:53:36 +0200

    Move crouch jump and fall to one script

---
+++ Assets/Scripts/PlayerCrouchAndJump.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerCrouchAndJump : MonoBehaviour
+    {
+        public CharacterController Controller;
+
+        public Transform GroundCheck;
+        public LayerMask GroundMask;
+        private float _groundDistance = 0.4f;
+
+        private Vector3 _velocity;
+        private float _gravityConst = -9.81f;
+        private float _jumpHeight = 0.0005f;
+
+        private bool _isGrounded;
+
+        private bool _crouching;
+        private float _originalHeight;
+        private float _crouchHeight;
+
+        public void Start()
+        {
+            _originalHeight = Controller.height;
+            _crouchHeight = _originalHeight * 0.3f;
+            _crouching = false;
+            _isGrounded = Physics.CheckSphere(GroundCheck.position, _groundDistance, GroundMask);
+        }
+        public void Update() 
+        {
+            UpdateCrouch();
+            UpdateJump();
+            UpdateFall();
+        }
+
+        private void UpdateJump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            {
+                _crouching = false;
+                UpdateCrouchHeight();
+
+                _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravityConst);
+            }
+        }
+
+        private void UpdateFall()
+        {
+            _isGrounded = Physics.CheckSphere(GroundCheck.position, _groundDistance, GroundMask);
+            if (_isGrounded && _velocity.y < 0)
+                _velocity.y = -1f;
+
+            _velocity.y += _gravityConst * Time.deltaTime * Time.deltaTime;
+            Controller.Move(_velocity);
+        }
+
+        private void UpdateCrouch()
+        {
+            if (Input.GetKeyDown(KeyCode.C) && _isGrounded)
+            {
+                _crouching = !_crouching;
+                UpdateCrouchHeight();
+            }
+        }
+
+        private void UpdateCrouchHeight()
+        {
+            Controller.height = _crouching ? _crouchHeight : _originalHeight;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController CharacterController;
-    public float Speed = 12f;
-    public Transform GroundCheck;
-    public float GroundDistance = 0.4f;
-    public LayerMask GroundMask;
-    public bool IsGrounded;
+    public CharacterController Controller;
 
-    private Vector3 _velocity;
-    private float _gravityConst = -9.81f;
-    private float _jumpHeight = 0.001f;
+    private float _speed = 12f;
     void Update()
     {
-        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
-
-        if (IsGrounded && _velocity.y < 0)
-            _velocity.y = -1f;
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 direction = transform.right * x + transform.forward * z;
 
-        CharacterController.Move(direction * Speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravityConst);
-
-        _velocity.y += _gravityConst * Time.deltaTime * Time.deltaTime;
-        CharacterController.Move(_velocity); 
+        Controller.Move(direction * _speed * Time.deltaTime);
     } 
 }
