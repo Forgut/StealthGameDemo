dec5b43be5e3c93bc3b0485b1a0133384de653e7
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-30 12:38:22 +0200

    Initail commit Add movement Add jumping Add crouching

---
+++ Assets/Scripts/PlayerCrouch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerCrouch : MonoBehaviour
+    {
+        public CharacterController Controller;
+        private bool _crouching;
+        private float _originalHeight;
+        private float _crouchHeight;
+
+        public void Start()
+        {
+            _originalHeight = Controller.height;
+            _crouchHeight = _originalHeight * 0.3f;
+            _crouching = false;
+        }
+        public void Update() { 
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                _crouching = !_crouching;
+                Controller.height = _crouching ? _crouchHeight : _originalHeight;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController CharacterController;
+    public float Speed = 12f;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    public bool IsGrounded;
+
+    private Vector3 _velocity;
+    private float _gravityConst = -9.81f;
+    private float _jumpHeight = 0.001f;
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (IsGrounded && _velocity.y < 0)
+            _velocity.y = -1f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.right * x + transform.forward * z;
+
+        CharacterController.Move(direction * Speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravityConst);
+
+        _velocity.y += _gravityConst * Time.deltaTime * Time.deltaTime;
+        CharacterController.Move(_velocity); 
+    } 
+}
